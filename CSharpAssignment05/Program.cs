ce57fa80f40e27a09acde29088ad4646b3fbb212
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-09 12:50:37 +0300

    Question01

--- CSharpAssignment05/Program.cs
+++ CSharpAssignment05/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //Book book = new Book();
+            //Console.WriteLine(book.Password);
+            ////It gives an error 'Book.Password' is inaccessible due to its protection level
+            ////Because we can't access private fields outside of the Book class
+            #endregion
+
         }
     }
+    #region GeneralClass
+    public class Book
+    {
+        private string Password = "secret";
+    }
+    #endregion
 }
