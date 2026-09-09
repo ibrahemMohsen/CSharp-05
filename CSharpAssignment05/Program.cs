f94914b3a39d4473254d775633bbf0775558d515
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-09 12:51:44 +0300

    Question02

--- CSharpAssignment05/Program.cs
+++ CSharpAssignment05/Program.cs
@@ -11,12 +11,18 @@
             ////Because we can't access private fields outside of the Book class
             #endregion
 
+            #region Question02
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock);
+            ////yes, because this class can access internal fields from inside the same project/assembly
+            #endregion
         }
     }
     #region GeneralClass
     public class Book
     {
         private string Password = "secret";
+        internal int copiesInStock = 5;
     }
     #endregion
 }
